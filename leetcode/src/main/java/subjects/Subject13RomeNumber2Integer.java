254b5a815959f9ea28aeba35a649064a5daa7415
youchao.wen <wb.wenyouchao@mesg.corp.netease.com>
2020-06-11 11:58:30 +0800

    leetcode刷题

--- leetcode/src/main/java/subjects/Subject13RomeNumber2Integer.java
+++ leetcode/src/main/java/subjects/Subject13RomeNumber2Integer.java
@@ -31,51 +31,52 @@ public class Subject13RomeNumber2Integer {
     static int value = 0;
     public static int romanToInt(String romeNumber){
 
-        if(romeNumber == null || romeNumber.length() == 0){
+        if(romeNumber == null || "".equals(romeNumber) || romeNumber.length() == 0){
             return value;
         }
-        int lastIndex = romeNumber.length() - 1;
-        String last2Str = romeNumber.length() > 1 ? romeNumber.substring(lastIndex - 1) : "";
+        int length = romeNumber.length();
+        int lastIndex = length - 1;
+        String last2Str = length > 1 ? romeNumber.substring(lastIndex - 1) : "";
         String lastStr = romeNumber.substring(lastIndex);
         if(last2Str.equals(four)){
             value += 4;
-            romanToInt(romeNumber.substring(0, lastIndex - 1));
+            return romanToInt(romeNumber.substring(0, length - 2));
         }else if(last2Str.equals(nine)){
             value += 9;
-            romanToInt(romeNumber.substring(0, lastIndex - 1));
+            return romanToInt(romeNumber.substring(0, length - 2));
         }else if(last2Str.equals(forty)){
             value += 40;
-            romanToInt(romeNumber.substring(0, lastIndex - 1));
+            return romanToInt(romeNumber.substring(0, length - 2));
         }else if(last2Str.equals(ninety)){
             value += 90;
-            romanToInt(romeNumber.substring(0, lastIndex - 1));
+            return romanToInt(romeNumber.substring(0, length - 2));
         }else if(last2Str.equals(forHundred)){
             value += 400;
-            romanToInt(romeNumber.substring(0, lastIndex - 1));
+            return romanToInt(romeNumber.substring(0, length - 2));
         }else if(last2Str.equals(nineHundred)){
             value += 900;
-            romanToInt(romeNumber.substring(0, lastIndex - 1));
+            return romanToInt(romeNumber.substring(0, length - 2));
         }else if(lastStr.equals(one)){
             value += 1;
-            romanToInt(romeNumber.substring(0, lastIndex));
+            return romanToInt(romeNumber.substring(0, length - 1));
         }if(lastStr.equals(five)){
             value += 5;
-            romanToInt(romeNumber.substring(0, lastIndex));
+            return romanToInt(romeNumber.substring(0, length - 1));
         }else if(lastStr.equals(ten)){
             value += 10;
-            romanToInt(romeNumber.substring(0, lastIndex));
+            return romanToInt(romeNumber.substring(0, length - 1));
         }else if(lastStr.equals(fifty)){
             value += 50;
-            romanToInt(romeNumber.substring(0, lastIndex));
+            return romanToInt(romeNumber.substring(0, length - 1));
         }else if(lastStr.equals(hundred)){
             value += 100;
-            romanToInt(romeNumber.substring(0, lastIndex));
+            return romanToInt(romeNumber.substring(0, length - 1));
         }else if(lastStr.equals(fiveHundred)){
             value += 100;
-            romanToInt(romeNumber.substring(0, lastIndex));
+            return romanToInt(romeNumber.substring(0, length - 1));
         }else if(lastStr.equals(thousand)){
             value += 1000;
-            romanToInt(romeNumber.substring(0, lastIndex));
+            return romanToInt(romeNumber.substring(0, length - 1));
         };
 
         return value;
---
+++ leetcode/src/main/java/subjects/Subject14LongestCommonPrefix.java
@@ -0,0 +1,11 @@
+package subjects;
+
+/**
+ * 最长通用前缀
+ *
+ * @author youchao.wen
+ * @date 2020/6/10.
+ */
+public class Subject14LongestCommonPrefix {
+
+}
