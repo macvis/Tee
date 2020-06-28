db9e495e45f6b001910b5cb78c915835048a5707
youchao.wen <wb.wenyouchao@mesg.corp.netease.com>
2020-06-28 11:58:20 +0800

    files save

---
+++ dubbo-spi/src/main/java/dubbo/spi/demo/MyProtocol.java
@@ -0,0 +1,36 @@
+package dubbo.spi.demo;
+
+import com.alibaba.dubbo.common.URL;
+import com.alibaba.dubbo.common.extension.Activate;
+import com.alibaba.dubbo.rpc.Exporter;
+import com.alibaba.dubbo.rpc.Invoker;
+import com.alibaba.dubbo.rpc.Protocol;
+import com.alibaba.dubbo.rpc.RpcException;
+
+/**
+ * @author youchao.wen
+ * @date 2020/6/15.
+ */
+@Activate("mine")
+public class MyProtocol implements Protocol {
+
+    @Override
+    public int getDefaultPort() {
+        return 0;
+    }
+
+    @Override
+    public <T> Exporter<T> export(Invoker<T> invoker) throws RpcException {
+        return null;
+    }
+
+    @Override
+    public <T> Invoker<T> refer(Class<T> type, URL url) throws RpcException {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
