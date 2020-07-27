10763b55fffb3648d0c4ba1da301b8ef4253391d
youchao.wen <macvis@126.com>
2020-07-27 16:50:03 +0800

    刷题

--- leetcode/src/main/java/subjects/Subject17DialKeysLettersComposition.java
+++ leetcode/src/main/java/subjects/Subject17DialKeysLettersComposition.java
@@ -27,26 +27,32 @@ public class Subject17DialKeysLettersComposition {
         if(digits.equals(""))
             return res;
 
-        findCombination(digits, 0, "");
+        findCombination(digits, "", 0);
         return res;
     }
 
-    private static void  findCombination(String digits, int index, String s){
-
-        if(index == digits.length()){
-            res.add(s);
+    private static void findCombination(String str, String letter, int index) {
+        //递归的终止条件，注意这里的终止条件看上去跟动态演示图有些不同，主要是做了点优化
+        //动态图中是每次截取字符串的一部分，"234"，变成"23"，再变成"3"，最后变成""，这样性能不佳
+        //而用index记录每次遍历到字符串的位置，这样性能更好
+        if(index == str.length()) {
+            res.add(letter);
             return;
         }
-
-        Character c = digits.charAt(index);
-        String letters = DIAL_LETTERS[c - '0'];
-        for(int i = 0 ; i < letters.length() ; i ++){
-            findCombination(digits, index+1, s + letters.charAt(i));
+        //获取index位置的字符，假设输入的字符是"234"
+        //第一次递归时index为0所以c=2，第二次index为1所以c=3，第三次c=4
+        //subString每次都会生成新的字符串，而index则是取当前的一个字符，所以效率更高一点
+        char c = str.charAt(index);
+        //map_string的下表是从0开始一直到9， c-'0'就可以取到相对的数组下标位置
+        //比如c=2时候，2-'0'，获取下标为2,letter_map[2]就是"abc"
+        int pos = c - '0';
+        String map_string = DIAL_LETTERS[pos];
+        //遍历字符串，比如第一次得到的是2，页就是遍历"abc"
+        for(int i=0;i<map_string.length();i++) {
+            //调用下一层递归，用文字很难描述，请配合动态图理解
+            findCombination(str, letter+map_string.charAt(i), index+1);
         }
-
-        return;
     }
-
 
     public static void main(String[] args) {
         System.out.println(solve("23"));
