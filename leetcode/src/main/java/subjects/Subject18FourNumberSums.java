16ac5e5040de291381b9d6da0e9a944b69621e2f
youchao.wen <macvis@126.com>
2021-02-19 13:21:03 +0800

    files save

---
+++ leetcode/src/main/java/subjects/Subject18FourNumberSums.java
@@ -0,0 +1,28 @@
+package subjects;
+
+/**
+ * 四数之和
+ *  给定一个包含n个整数的数组nums和一个目标值target，
+ *  判断nums中是否存在四个元素 a，b，c和 d，
+ *  使得a + b + c + d的值与target相等？找出所有满足条件且不重复的四元组。
+ *  注意：
+ *      答案中不可以包含重复的四元组。
+ *  示例：
+ *      给定数组 nums = [1, 0, -1, 0, -2, 2]，和 target = 0。
+ *          满足要求的四元组集合为：
+ *              [
+ *                  [-1,  0, 0, 1],
+ *                  [-2, -1, 1, 2],
+ *                  [-2,  0, 0, 2]
+ *              ]
+ *
+ * @author youchao.wen
+ * @date 2021/2/3.
+ */
+public class Subject18FourNumberSums {
+    private int[] solve(int[] array, int target){
+
+
+        return null;
+    }
+}
